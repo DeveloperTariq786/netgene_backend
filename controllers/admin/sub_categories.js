a6308fad69066078c3656f9e076fa4175878acc9
faizan ahmad mir <faizan.mir@acmo.in>
2025-11-23 22:12:59 +0530

    sub-category controller added

--- controllers/admin/sub_categories.js
+++ controllers/admin/sub_categories.js
@@ -1,16 +1,17 @@
 import Subcategory from "../../models/sub_category.model.js";
 import mongoose from "mongoose";
-
+import {uploadToFirebaseStorage} from "../../helpers/uploadtofirebase.js";
+import Category from "../../models/category.model.js";
 const addSubCategory = async(req,res)=>{
     try{
         console.log("Add sub-category route was hit");
         const {sub_category_name} = req.body;
         const category_id = req.query.C_ID;
          
-     if(!sub_category_name){
+     if(!sub_category_name || !req.file){
         return res.status(403).json({
             success:false,
-            message:"Please enter sub_category name"
+            message:"All fields are required"
         })
      }
      if(!category_id || !mongoose.Types.ObjectId.isValid(category_id)){
@@ -19,17 +20,43 @@ const addSubCategory = async(req,res)=>{
             message:"Please Enter Category Id or Enter valid category id"
         })
      }
+      // checking whether parent category exists or not:
+      let parentCategory  = await Category.findById(category_id);
+        if(!parentCategory){
+          console.log("Please Enter correct Category Id");
+          return res.status(404).json({
+            success:false,
+            message:"Category Id not found please enter latest category id"
+          })
+        }        
+
+     const existingSubCategory = await Subcategory.findOne({sub_category_name:sub_category_name.toLowerCase()});
+     if(existingSubCategory){
+      return res.status(403).json({
+        success:false,
+        message:"Sub-Category already exists no need to insert it"
+      })    
+
+     }
     
-    const originalNameOfImage = req.file?.originalname;
-    const destination = req.file?.destination;
-    const imageUrl = `${destination}/${originalNameOfImage}`;
-    console.log("Final Image Url",imageUrl);
+    // const originalNameOfImage = req.file?.originalname;
+    // const destination = req.file?.destination;
+    // const imageUrl = `${destination}/${originalNameOfImage}`;
+    // console.log("Final Image Url",imageUrl);
      
     // Adding Subcategory:
+     const fileName = `assets/${req.file.originalname}`;
+        const logo_url = await uploadToFirebaseStorage(
+            req.file.buffer,
+            fileName,
+            req.file.mimetype
+          );
+
+
 
     const addSubCategory = await new Subcategory({
       sub_category_name:sub_category_name?.toLowerCase(),
-      sub_category_logo:imageUrl,
+      sub_category_logo:logo_url,
       parent_category:category_id
 
     }).save();
@@ -54,6 +81,7 @@ const addSubCategory = async(req,res)=>{
 
 
     }catch(err){
+      console.log("Error occured while adding Sub-category",err);
       return res.status(501).json({
         success:false,
         message:"Error occured while adding Sub-category"
