07d89b89afdfc9ded17489604287521d61522b2c
faizan ahmad mir <faizan.mir@acmo.in>
2025-11-23 22:12:34 +0530

    category controller updated

--- controllers/admin/categories.js
+++ controllers/admin/categories.js
@@ -1,31 +1,44 @@
 import Category from "../../models/category.model.js";
-
+import {uploadToFirebaseStorage} from "../../helpers/uploadtofirebase.js"
 const addCategory = async(req,res)=>{
   try{
-    console.log("Add category route was hit",req.file);
+    console.log("Add category route was hit");
     const {category_name} = req.body;
-     if(!category_name){
+     if(!category_name || !req.file){
         return res.status(401).json({
             success:false,
-            message:"Please enter category name"
+            message:"All fields are required"
         })
      }
-    
-    const originalNameOfImage = req.file?.originalname;
-    const destination = req.file?.destination;
-    const imageUrl = `${destination}/${originalNameOfImage}`;
-    console.log("Final Image Url",imageUrl);
+     // Checking whether category exists:
+     const existingCategory = await Category.findOne({category_name:category_name.toLowerCase()});
+     if(existingCategory){
+      return res.status(403).json({
+        success:false,
+        message:"Category already exists no need to insert it"
+      })    
+
+     }    
+    // const originalNameOfImage = req.file?.originalname;
+    // const destination = req.file?.destination;
+    // const imageUrl = `${destination}/${originalNameOfImage}`;
+    // console.log("Final Image Url",imageUrl);
      
     // Adding Category:
-
+    const fileName = `assets/${req.file.originalname}`;
+        const logo_url = await uploadToFirebaseStorage(
+            req.file.buffer,
+            fileName,
+            req.file.mimetype
+          );
     const addCategory = await new Category({
       category_name:category_name?.toLowerCase(),
-      category_logo:imageUrl
+      category_logo:logo_url
 
     }).save();
 
     if(addCategory){
-      console.log("Category was added");  
+      console.log("Category was added successfully");  
       return res.status(201).json({
         success:true,
         message:"Category was added successfully",
