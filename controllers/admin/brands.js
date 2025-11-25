1e10e80e2d79cb967bb9329e84bf55860f0ef63d
faizan ahmad mir <faizan.mir@acmo.in>
2025-11-25 20:59:25 +0530

    all routes created

--- controllers/admin/brands.js
+++ controllers/admin/brands.js
@@ -1,5 +1,6 @@
 import Brand from "../../models/brand.model.js";
 import {uploadToFirebaseStorage} from "../../helpers/uploadtofirebase.js";
+import { mongoose } from 'mongoose';
 
 const addBrand = async(req,res)=>{
   try{
@@ -62,4 +63,142 @@ const addBrand = async(req,res)=>{
   }
 }
 
-export {addBrand}
+const updateBrand = async(req,res)=>{
+  try{
+    let brandObj = {};
+    console.log("Update brand was hit");
+    const {brand_name} = req.body;
+    brandObj.brand_name = brand_name.toLowerCase(); 
+    const brand_id = req.query.brand_id;    
+     if(!brand_id || !mongoose.Types.ObjectId.isValid(brand_id)){
+       console.log("Please select brand or brand not valid");
+       return res.status(403).json({
+        success:false,
+        message:"Invalid brand or select brand"
+       })
+     }
+     // checking whether brand exists :
+     const checkBrand = await Brand.findById(brand_id);  
+     if(!checkBrand){
+        return res.status(403).json({
+          success:false,
+          message:"Brand does not exists please add brand first"
+        })
+
+     }
+      //
+      let logo_url;
+      if(req?.file){    
+     const fileName = `assets/${req.file.originalname}`;
+      logo_url = await uploadToFirebaseStorage(
+        req.file.buffer,
+        fileName,
+        req.file.mimetype
+      );
+       }
+       // preparing brand to get updated:
+         const brandFilter = {_id:brand_id};
+         if(logo_url){
+            brandObj.brand_logo = logo_url;          
+         } 
+      let updateBrand = await Brand.updateOne(brandFilter,{
+                                              $set:brandObj     
+      });
+      if(updateBrand){
+        console.log("Brand updated successfully");
+        return res.status(201).json({
+          success:true,
+          message:"Brand updated successfully",
+        })
+      }else{
+         return res.status(404).json({
+          success:false,
+          message:"Brand was not updated"
+         })
+      }
+
+     
+  }
+  catch(err){
+   console.log("Error occured while updating the brand",err);
+    return res.status(501).json({
+      success:false,
+      message:"Error occured while updating the brand"
+    })
+
+  }
+}
+
+const getBrand = async(req,res)=>{
+  try{
+    console.log("Get brand was hit");
+    const {brand_id} = req.query;
+    if(!brand_id || !mongoose.Types.ObjectId.isValid(brand_id)){
+      return res.status(403).json({
+        success:false,
+        message:"Select brand or invalid brand"
+      });
+    }
+
+    let getSingleBrand = await Brand.findById(brand_id).select({_id:1,brand_name:1,brand_logo:1});
+    if(getSingleBrand){
+        console.log("Brand Fetched Successfully");
+        return res.status(200).json({
+          success:true,
+          message:"Brand found Successfully",
+          Brand:getSingleBrand
+        })
+    }
+    else{
+      console.log("Brand was not found!");
+        return res.status(404).json({
+          success:false,
+          message:"Brand was not found",
+        })
+    }
+
+
+
+  }catch(err){
+    console.log("Error occured while fetching single brand",err);
+    return res.status(501).json({
+      success:false,
+      message:"Error occured while fetching single brand"
+    })
+
+  }
+}
+const getAllBrands = async(req,res)=>{
+  try{
+    console.log("Get all brands was hit");
+    const data = await Brand.find().select({_id:1,brand_name:1,brand_logo:1}).sort({createdAt:-1});
+     if(data.length){
+        console.log("Brands fetched successfully");
+        return res.status(200).json({
+          success:true,
+          message:"Brands fetched successfully",
+          allBrands:data
+        });
+
+     }
+     else{
+        console.log("Brands not found please add some brands first");
+
+         return res.status(404).json({
+          success:false,
+          message:"Brands not found please add some brands first",
+        });
+     }
+
+  }
+  catch(err){
+    console.log("Error occured while fetching all brands",err);
+    return res.status(501).json({
+      success:false,
+      message:"Error occured while fetching all brands"
+    })
+  }
+
+}
+
+export {addBrand,updateBrand,getBrand,getAllBrands}
