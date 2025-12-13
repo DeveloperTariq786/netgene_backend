3bd064f8724b55e4973a1683906566418fa2b8b0
faizan ahmad mir <faizan.mir@acmo.in>
2025-12-13 22:38:15 +0530

    inventory model created

---
+++ models/inventory.model.js
@@ -0,0 +1,24 @@
+import mongoose from "mongoose";
+
+const InventorySchema = mongoose.Schema({
+    product_id:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"Product"
+    },
+    product_stock:{
+        type:Number,
+        required:true,
+        default:0
+    },
+    product_code:{
+        type:String,
+        // unique:true,
+        // index:1
+    },
+    stock_status:{
+        type:String,
+        enum:["In Stock","Low Of Stock","Out Of Stock"]
+    }
+},{timestamps:true});
+const Inventory = mongoose.model("Inventory",InventorySchema);
+export default Inventory;
