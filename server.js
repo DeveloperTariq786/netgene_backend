f9b836389b287638ae34df9cd8d6d474c329d31b
Faizan Mir <faizan.mir@acmo.ingit config --global --listgit config --global user.email faizan.mir@acmo.in>
2025-12-18 22:22:27 +0530

    customer routes enabled

--- server.js
+++ server.js
@@ -1,35 +1,39 @@
 import dotenv from "dotenv";
-dotenv.config();  
+dotenv.config();
 import express from "express";
-import {router as adminRouter} from "./routes/admin/api/v1/admin.routes.js";
+import { router as adminRouter } from "./routes/admin/api/v1/admin.routes.js";
+import { router as customerRouter } from "./routes/customer/api/v1/customer.routes.js"
 import connectDb from "./config/db/connect.js";
 import cors from "cors";
 const app = express();
 app.use(cors());
-connectDb().then(()=>{
+connectDb().then(() => {
   console.log("Connected to the db successfully");
-}).catch((err)=>{
+}).catch((err) => {
   console.log(` error occured while connecting db ${err}`);
 })
 
 // Midllewares to parse JSON:    
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 // Middleware to get path to the public folder where we store images/pdf's,etc:
-app.use(express.static('public')); 
+app.use(express.static('public'));
 const PORT = process.env.PORT_NUMBER || 3000;
 
 // possible routes:
 
-app.get('/',(req,res)=>{
+app.get('/', (req, res) => {
   console.log("Home Route Was Hit");
-  res.send("<h1> Welcome to the Ecomm App <h1/>");  
+  res.send("<h1> Welcome to the Ecomm App <h1/>");
 })
 // rest end-points:
 // admin routes:
-app.use('/api/v1/admin',adminRouter);
+app.use('/api/v1/admin', adminRouter);
+// customer routes:
+app.use('/api/v1/customer', customerRouter);
 
-app.listen(PORT,()=>{
-   console.log(`Listening to the portNumber ${PORT}`); 
+
+app.listen(PORT, () => {
+  console.log(`Listening to the portNumber ${PORT}`);
 })
 
