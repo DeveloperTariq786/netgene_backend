c18948c6e4fe0e9ebc27435261677058c1817802
Faizan Mir <faizan.mir@acmo.ingit config --global --listgit config --global user.email faizan.mir@acmo.in>
2025-12-18 22:22:00 +0530

    product model modified

--- models/product.model.js
+++ models/product.model.js
@@ -1,107 +1,110 @@
 import mongoose from "mongoose";
 const ImageSchema = new mongoose.Schema({
-    url:{
-        type:String,   
+    url: {
+        type: String,
     }
 
 });
-const ReviewsSchema =new mongoose.Schema({
-   commented_by:{
-    type:mongoose.Schema.Types.ObjectId,
-    ref:"User",
-      
-   },
-   comments:{
-    type:String
-   }    
+const ReviewsSchema = new mongoose.Schema({
+    commented_by: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+
+    },
+    comments: {
+        type: String
+    }
 
 });
 const LikedAndUnlikedSchema = new mongoose.Schema({
-   likedBy:{
-    type:mongoose.Schema.Types.ObjectId,
-    ref:"User",
-    isLiked:Boolean
-   } 
+    likedBy: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        isLiked: Boolean
+    }
 })
 const TagSchema = new mongoose.Schema({
-    tag_name: {type:String}
+    tag_name: { type: String }
 })
 
 const ProductSchema = new mongoose.Schema({
-product_name:{
-    type:String,
-    required:true
-},
-product_description:{
-    type:String,
-    default:"Not Known"
-},
-product_price:{
-    type:Number,
-    required:true,
-    default:0
-},
-discount_precentage:{
-    type:Number,
-    required:true,
-    default:0
-},
-final_price:{
-    type:Number,
-    
-},
-product_brand:{
-    type:mongoose.Schema.Types.ObjectId,
-    ref:"Brand"
-},
-product_category:{
-    type:mongoose.Schema.Types.ObjectId,
-    ref:"Category"
-},
-product_sub_category:{
-    type:mongoose.Schema.Types.ObjectId,
-    ref:"Subcategory"
-},
-avatar:{
-    type:String,
-       
-},
-cover_images:[ImageSchema],
-product_reviews:[ReviewsSchema],
-product_likes:[LikedAndUnlikedSchema],
-added_by:{
-    type:mongoose.Schema.Types.ObjectId,
-    ref:"User",
-},
-updated_by:{
-    type:mongoose.Schema.Types.ObjectId,
-    ref:"User",
-},
-dimensions:{
-    type:mongoose.Schema.Types.ObjectId,
-    ref:"Metrics"
-},
-manufacturer:{
-    type:String
-},
-sales:{
-    type:Boolean,
-    required:true
-},
-featured:{
-    type:Boolean,
-    required:true
-},
-tags:[TagSchema],
-created_by:{
-    type:mongoose.Schema.Types.ObjectId,
-    ref:"User"
-},
-updated_by:{
-    type:mongoose.Schema.Types.ObjectId,
-    ref:"User"
-}
-},{timestamps:true});
+    product_name: {
+        type: String,
+        required: true
+    },
+    product_description: {
+        type: String,
+        default: "Not Known"
+    },
+    product_price: {
+        type: Number,
+        required: true,
+        default: 0
+    },
+    discount_precentage: {
+        type: Number,
+        required: true,
+        default: 0
+    },
+    final_price: {
+        type: Number,
 
-const Product = mongoose.model("Product",ProductSchema);
+    },
+    product_brand: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Brand"
+    },
+    product_category: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Category"
+    },
+    product_sub_category: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Subcategory"
+    },
+    avatar: {
+        type: String,
+
+    },
+    cover_images: [ImageSchema],
+    product_reviews: [ReviewsSchema],
+    product_likes: [LikedAndUnlikedSchema],
+    added_by: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+    },
+    updated_by: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+    },
+    dimensions: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Metrics"
+    },
+    manufacturer: {
+        type: String
+    },
+    sales: {
+        type: String,
+        required: true
+    },
+    featured: {
+        type: String,
+        required: true
+    },
+    isNew: {
+        type: String,
+    },
+    tags: [TagSchema],
+    created_by: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User"
+    },
+    updated_by: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User"
+    }
+}, { timestamps: true });
+
+const Product = mongoose.model("Product", ProductSchema);
 export default Product;
