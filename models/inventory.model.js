fac72f3060c688aae59e6ce812020d044f555649
Faizan Mir <faizan.mir@acmo.ingit config --global --listgit config --global user.email faizan.mir@acmo.in>
2025-12-18 22:21:42 +0530

    inventory updated

--- models/inventory.model.js
+++ models/inventory.model.js
@@ -1,24 +1,27 @@
 import mongoose from "mongoose";
 
 const InventorySchema = mongoose.Schema({
-    product_id:{
-        type:mongoose.Schema.Types.ObjectId,
-        ref:"Product"
+    product_id: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Product"
     },
-    product_stock:{
-        type:Number,
-        required:true,
-        default:0
+    product_stock: {
+        type: Number,
+        required: true,
+        default: 0
     },
-    product_code:{
-        type:String,
+    product_code: {
+        type: String,
         // unique:true,
         // index:1
     },
-    stock_status:{
-        type:String,
-        enum:["In Stock","Low Of Stock","Out Of Stock"]
+    stock_status: {
+        type: String,
+        enum: ["In Stock", "Low Of Stock", "Out Of Stock"]
+    },
+    date: {
+        type: String
     }
-},{timestamps:true});
-const Inventory = mongoose.model("Inventory",InventorySchema);
+}, { timestamps: true });
+const Inventory = mongoose.model("Inventory", InventorySchema);
 export default Inventory;
