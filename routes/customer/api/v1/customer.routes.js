a058f06d3bfca6562118c8fbec91ed1670b8a1cb
Faizan Mir <faizan.mir@acmo.ingit config --global --listgit config --global user.email faizan.mir@acmo.in>
2025-12-19 20:22:17 +0530

    more routes added

--- routes/customer/api/v1/customer.routes.js
+++ routes/customer/api/v1/customer.routes.js
@@ -3,9 +3,11 @@ const router = express.Router();
 
 // Dashboard routes:
 
-import { getBrandsWithProducts, getCategoriesWithSubCategories } from "../../../../controllers/customer/dashboard/dashboard.js";
+import { fetchNewProducts, getBrandsWithProducts, getCategoriesWithSubCategories, getFeaturedProducts } from "../../../../controllers/customer/dashboard/dashboard.js";
 
 router.get('/brands', getBrandsWithProducts);
 router.get('/categories', getCategoriesWithSubCategories);
+router.get('/featured', getFeaturedProducts);
+router.get('/new', fetchNewProducts);
 
 export { router };
