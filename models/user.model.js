b7b2719cb6da08de58e5ab1067938b2461c64a58
Faizan Mir <faizan.mir@acmo.ingit config --global --listgit config --global user.email faizan.mir@acmo.in>
2025-12-24 11:50:30 +0530

    user model updated

--- models/user.model.js
+++ models/user.model.js
@@ -1,77 +1,81 @@
 import mongoose, { Schema } from "mongoose";
 import bcrypt from "bcryptjs";
 
-const PermissionSchema  = new Schema({
-  can_add_superadmin:{
-   type:Boolean,
-   default:true,
-  },
-  can_add_admin:{
-   type:Boolean,
-   default:true,
-  },
-  can_add_records:{
-   type:Boolean,
-   default:true,
-  },
-  can_update_records:{
-   type:Boolean,
-   default:true,
-  },
-  can_read_records:{
-   type:Boolean,
-   default:true,
-  },
-  can_delete_records:{
-   type:Boolean,
-   default:true,
-  },
-})
-
-const UserSchema  = new mongoose.Schema({
-   first_name:{
-    type:String,
-    required:true
+const PermissionSchema = new Schema({
+   can_add_superadmin: {
+      type: Boolean,
+      default: true,
    },
-   last_name:{
-    type:String,
-    required:true
+   can_add_admin: {
+      type: Boolean,
+      default: true,
    },
-   phone_number:{
-    type:String,
-    required:true 
+   can_add_records: {
+      type: Boolean,
+      default: true,
    },
-   email:{
-    type:String,
-    required:true
+   can_update_records: {
+      type: Boolean,
+      default: true,
    },
-   password:{
-    type:String,
-    required:true
+   can_read_records: {
+      type: Boolean,
+      default: true,
    },
-   role:{
-    type:String,
-    enum:['admin','customer',"superadmin"],
-    required:true
+   can_delete_records: {
+      type: Boolean,
+      default: true,
    },
-   permission_component:[PermissionSchema]           
-
-
-
-},{timestamps:true});
-UserSchema.pre("save",async function(next){
-   if(!this.isModified('password')){
-      return next();
-  
+   is_customer: {
+      type: Boolean,
+      default: false
    }
-    this.password = await bcrypt.hash(this.password,10); 
-    next();
 })
-UserSchema.methods.isPasswordCorrect = async function (password){
-  // console.log("Password Hash is Schema",password);
-           return await bcrypt.compare(password, this.password);
+
+const UserSchema = new mongoose.Schema({
+   first_name: {
+      type: String,
+      required: true
+   },
+   last_name: {
+      type: String,
+      required: true
+   },
+   phone_number: {
+      type: String,
+      //  required:true 
+   },
+   email: {
+      type: String,
+      required: true
+   },
+   password: {
+      type: String,
+      required: true
+   },
+   role: {
+      type: String,
+      enum: ['admin', 'customer', "superadmin"],
+      required: true
+   },
+   permission_component: [PermissionSchema]
+
+
+
+}, { timestamps: true });
+UserSchema.pre("save", async function (next) {
+   if (!this.isModified('password')) {
+      return next();
+
+   }
+   this.password = await bcrypt.hash(this.password, 10);
+   next();
+})
+UserSchema.methods.isPasswordCorrect = async function (password) {
+   // console.log("Password Hash is Schema",password);
+   return await bcrypt.compare(password, this.password);
 }
 
-const User = mongoose.model("User",UserSchema);
+const User = mongoose.model("User", UserSchema);
 
 export default User;
