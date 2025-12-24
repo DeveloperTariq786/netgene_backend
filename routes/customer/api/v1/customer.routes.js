f9db591204c46a14874b594828ec36bee9284a0e
Faizan Mir <faizan.mir@acmo.ingit config --global --listgit config --global user.email faizan.mir@acmo.in>
2025-12-24 11:50:39 +0530

    routes updated

--- routes/customer/api/v1/customer.routes.js
+++ routes/customer/api/v1/customer.routes.js
@@ -1,13 +1,32 @@
 import express from "express";
 const router = express.Router();
+import { authenticateJWT } from "../../../../middlewares/authenticate.routes.js";
+import { fetchNewProducts, getBrandsWithProducts, getCategoriesWithSubCategories, getFeaturedProducts } from "../../../../controllers/customer/dashboard/dashboard.js";
+import { loginAndRegister } from "../../../../controllers/customer/customer.login.js";
+import { addToCart, fetchCartItems } from "../../../../controllers/customer/cart.js";
+import { placeOrder } from "../../../../controllers/customer/order.js";
+import { addShippingAddress } from "../../../../controllers/customer/order.address.js";
+
+
+// customer login route:
+router.post('/login', loginAndRegister)
+
 
 // Dashboard routes:
-
-import { fetchNewProducts, getBrandsWithProducts, getCategoriesWithSubCategories, getFeaturedProducts } from "../../../../controllers/customer/dashboard/dashboard.js";
-
 router.get('/brands', getBrandsWithProducts);
 router.get('/categories', getCategoriesWithSubCategories);
 router.get('/featured', getFeaturedProducts);
 router.get('/new', fetchNewProducts);
+
+// Cart routes:
+router.post('/add-cart', authenticateJWT, addToCart);
+router.get('/cart', authenticateJWT, fetchCartItems);
+
+// Order routes:
+
+router.post('/order', authenticateJWT, placeOrder);
+
+// Shipping Address routes:
+router.post('/address', authenticateJWT, addShippingAddress)
 
 export { router };
