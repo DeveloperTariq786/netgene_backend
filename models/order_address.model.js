33c009c95f5baecfd35def4503948a103e1c62cb
Faizan Mir <faizan.mir@acmo.ingit config --global --listgit config --global user.email faizan.mir@acmo.in>
2025-12-24 11:50:12 +0530

    shipping address model created

---
+++ models/order_address.model.js
@@ -0,0 +1,49 @@
+import mongoose from "mongoose";
+
+const OrderAddressSchema = new mongoose.Schema({
+
+    customer_id: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: true
+    },
+    first_name: {
+        type: String,
+        required: true
+    },
+    last_name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    phone_number: {
+        type: String,
+        required: true
+    },
+    address: {
+        type: String,
+        required: true
+    },
+    country: {
+        type: String,
+        required: true
+    },
+    state: {
+        type: String,
+        required: true
+    },
+    city: {
+        type: String,
+        required: true
+    },
+    postal_code: {
+        type: String,
+        required: true
+    }
+
+}, { timestamps: true });
+const OrderAddress = mongoose.model("OrderAddress", OrderAddressSchema);
+export default OrderAddress;
